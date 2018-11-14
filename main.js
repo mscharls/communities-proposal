9751dfe8645fe7a720d8efb678fe286d1b5a44c6
Charlotte <charlottemaryhailey@gmail.com>
2018-11-14 19:02:09 +0800

    content started, menu configured

--- main.js
+++ main.js
@@ -11,7 +11,115 @@
 //
 // });
 $( document ).ready(function() {
+
 $(".dropdown-nav").click(function(){
-    $(this).children(".dropdown-content").slideToggle("200");
+  if($(".dropdown-nav").attr("id") != "down"){
+    $(this).siblings(".dropdown-content").slideDown("200");
+     $(".dropdown-nav").attr("id","down");
+  }
+
+  else if($(".dropdown-nav").attr("id") === "down") {
+    $(this).siblings(".dropdown-content").slideUp("200");
+    $(".dropdown-nav").removeAttr("id");
+
+  }
+
   });
+
+  $("#letter_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").removeClass("hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#phases_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").removeClass("hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#services_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").removeClass("hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#timelines_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").removeClass("hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#budget_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").removeClass("hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#t_and_c_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").removeClass("hide");
+       $("#accept").attr("class","hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#accept_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").removeClass("hide");
+       $("#book").attr("class","hide");
+    });
+
+  $("#book_button").click(function(){
+      console.log("button clicked"); //debug comments
+       $("#letter").attr("class","hide");
+       $("#phases").attr("class","hide");
+       $("#services").attr("class","hide");
+       $("#timelines").attr("class","hide");
+       $("#budget").attr("class","hide");
+       $("#t_and_c").attr("class","hide");
+       $("#accept").attr("class","hide");
+       $("#book").removeClass("hide");
+    });
+
 });
