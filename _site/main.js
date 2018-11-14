31e74b624890955afe2c57e54fe28225f3610b5a
Charlotte <charlottemaryhailey@gmail.com>
2018-11-14 11:49:42 +0800

    jekyll-ified

--- _site/main.js
+++ _site/main.js
@@ -1,12 +1,17 @@
-
-$(document).ready(function() {
- $(".dropdown-nav").click(function(){
-    console.log("js running");
-    $(".dropdown-nav").removeClass("active");
-    $(this).addClass("active");
-    $(".dropdown-nav").not(".active").children(".dropdown-content").slideUp("200");
-    $(".dropdown-nav.active").children(".dropdown-content").slideDown("200");
+//
+// $(document).ready(function() {
+//  $(".dropdown-nav").click(function(){
+//     console.log("js running");
+//     $(".dropdown-nav").removeClass("active");
+//     $(this).addClass("active");
+//     $(".dropdown-nav").not(".active").children(".dropdown-content").slideUp("200");
+//     $(".dropdown-nav.active").children(".dropdown-content").slideDown("200");
+//   });
+//
+//
+// });
+$( document ).ready(function() {
+$(".dropdown-nav").click(function(){
+    $(this).children(".dropdown-content").slideToggle("200");
   });
-  
-
 });
